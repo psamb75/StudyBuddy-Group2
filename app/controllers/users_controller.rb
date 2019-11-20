6fa365c053e201bdf07bccf1b6d54b22f4b57ae8
Danting <danting.dong@mail.utoronto.ca>
2019-11-19 21:25:47 -0500

    Adding user information editing action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,26 @@
 class UsersController < ApplicationController
     #do we need auth part for it?
     def show
-        @me = current_user
+        @user = current_user
+        @my_courses = @user.enrolments
     end
     
     def edit
         @user = current_user
+    end
+
+    def update
+        last_name_ =  params[:user][:last_name]
+        first_name_ =  params[:user][:first_name]
+        phone_num_ = params[:user][:phone_num]
+        description_ = params[:user][:description]
+        @user = current_user
+        @user.update_attribute(:last_name, last_name_)
+        @user.update_attribute(:first_name, first_name_)
+        @user.update_attribute(:phone_num, phone_num_)
+        @user.update_attribute(:description, description_)
         
-        #check current pw
-        #check the two new pw matches or not
-        
-        
+        @my_courses = @user.enrolments
+        render 'show'
     end
 end
