a326eef8c823a43f095145d01ffb1b7c1b104b5b
Amir Mousavi <amir.mousavibiuki@mail.utoronto.ca>
2019-12-01 18:20:16 -0500

    Update the user profile page to show past payments, courses created and tutoring sessions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,18 @@ class UsersController < ApplicationController
     #do we need auth part for it?
     def show
         @user = User.find(params[:id])
-        @my_courses = @user.enrolments
+        @my_courses = @user.courses
+        my_tutoring_as_tutor = @user.tutors
+        my_tutoring_as_student = @user.tutoring_sessions
+        @my_active_tutoring_as_student = my_tutoring_as_student.select{ |s| s.completed == false }
+        @my_past_tutoring_as_student = my_tutoring_as_student.select{ |s| s.completed == true }
+        @my_active_tutoring_as_tutor = my_tutoring_as_tutor.select{ |s| s.completed == false }
+        @my_past_tutoring_as_tutor = my_tutoring_as_tutor.select{ |s| s.completed == true }
+
+        # payment history
+        require 'stripe'
+        Stripe.api_key = Rails.configuration.stripe[:secret_key]
+        @payment_history = Stripe::Charge.list(customer: current_user.card_token)
     end
     
     def edit
