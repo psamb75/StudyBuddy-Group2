c79ca2262b0be8b0822974563dad45d636a3bea0
Amir Mousavi <amir.mousavibiuki@mail.utoronto.ca>
2019-12-01 19:20:58 -0500

    Added notification for when a users comment is replied to.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,18 @@
 class Comment < ApplicationRecord
   validates :text, presence: true
+  after_create :create_notifications
 
   belongs_to :user
   belongs_to :note
+
+  private
+  def create_notifications
+    if self.ancestry != 0
+      recipient = Comment.find(self.ancestry).user
+      note = self.note
+      course = note.course
+      Notification.create(recipient: recipient, actor: self.user,
+        action: "#{self.user.name} replied to your comment on note #{note.title} in course #{course.course_name}", notifiable: self)
+    end
+  end
 end
