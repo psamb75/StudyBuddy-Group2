3745c7ddb3e65ff5300241f3030a4ad470c9445c
Amir Mousavi <amir.mousavibiuki@mail.utoronto.ca>
2019-12-03 10:56:21 -0500

    Add notification for when a user attends your study group. Fix the sign in page to display the error messages in case of wrong input credentials.

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,4 +1,12 @@
 class Attendee < ApplicationRecord
+  after_create :create_notifications
+
   belongs_to :user
   belongs_to :study_group
+
+  private
+  def create_notifications
+    Notification.create(recipient: self.study_group.user, actor: self.user,
+      action: "#{self.user.name} is attending your study group, #{self.study_group.event_name} in #{self.study_group.course.course_name}", notifiable: self)
+  end
 end
